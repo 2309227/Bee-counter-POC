e19f0a1c5bad25982a347af8e03eb01a1dce2d9a
Cameron Mitchell <2309227@uad.ac.uk>
2026-01-18 15:07:16 +0000

    Update Counter.py
    Remove uncessary files
    Clean up LED control
    Add comments
    Re order program
    Fixed calling main function

--- Counter.py
+++ Counter.py
@@ -6,6 +6,7 @@ import os
 
 # Import and set up relevent GPIO pins on Pi
 import RPi.GPIO as GPIO
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
@@ -13,75 +14,54 @@ GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 ledTrigger = "/sys/class/leds/ACT/trigger"
 ledBrightness = "/sys/class/leds/ACT/brightness"
 
-# Functions to control Pi zero built in LED
+
 def TurnLEDOn():
-    os.system(f"echo 1 | sudo tee {ledBrightness}")
+    with open(ledBrightness, "w") as f:
+        f.write("1")
 
 
 def TurnLEDOff():
-    os.system(f"echo 0 | sudo tee {ledBrightness}")
+    with open(ledBrightness, "w") as f:
+        f.write("0")
+
 
 # Wait until the pin is HIGH before starting
-os.system(f"echo none | sudo tee {ledTrigger}")
+os.system(f"echo none | sudo tee {ledTrigger} > /dev/null")
 
-print("Waiting for pin 11 to go HIGH to start...")
-
-# Loop until pin 11 is high 
+# Loop until pin 11 is high
 while GPIO.input(11) != GPIO.HIGH:
     TurnLEDOff()
     time.sleep(0.05)
 
-print("Pin is HIGH, starting detection...")
 TurnLEDOn()
 
 prev_state = GPIO.input(11)
 
 
-# Main driver function
-def main():
-
-    # Infanitie loop
-    while True:
-
-        run = CheckTrigger()
-        if run:
-            Currtime = GetTimestamp()
-            start = time.time_ns()
-            SaveToFile(Currtime)
-            end = time.time_ns()
-
-            print(end - start)
-
-
-# Trigger function (Button)
+# Trigger function
 def CheckTrigger():
-    global prev_state  # allow modifying the global variable
+    global prev_state
 
     current_state = GPIO.input(11)
     triggered = False
 
     # Trigger only once when pin goes from HIGH to LOW
     if prev_state == GPIO.HIGH and current_state == GPIO.LOW:
-        os.system(f"echo 0 | sudo tee {ledBrightness}")
+        TurnLEDOff()
         triggered = True
-        print("Contact lost!")  # single output
 
+    # If voltage is detected turn on LED
     elif current_state == GPIO.HIGH:
-        os.system(f"echo 1 | sudo tee {ledBrightness}")
+        TurnLEDOn()
 
     prev_state = current_state
     return triggered
 
 
-# Function to save last 10 items in q to file (CSV or TXT)
+# Function to save passed in string to CSV file
 def SaveToFile(Savedstring):
-
     with open("log.csv", "a") as f:
         f.write(Savedstring + "\n")
-
-    # with open("log.txt", "a") as f:
-    # for each item in que or just passed in value
-    #    f.write(Savedstring)
 
 
 # Function to get current timestamp (Will need modification for RTC use)
@@ -89,8 +69,18 @@ def GetTimestamp():
     return str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
 
+# Main driver function
+def main():
 
+    # Infanitie loop
+    while True:
+        # Check if IR has been tripped
+        run = CheckTrigger()
+        if run:
+            Currtime = GetTimestamp()  # Get time stamp
+            SaveToFile(Currtime)  # Save time stamp to csv file
 
 
 # Call main function
-main()
+if __name__ == "__main__":
+    main()
